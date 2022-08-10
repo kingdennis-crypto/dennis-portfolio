90aeac08f2341eaa0eecaafb6d25f6364bc8a00c
Dennis Moes <dennismoes@me.com>
2022-08-10 18:40:00 +0200

    Undo

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,5 @@ module.exports = {
     title: `Dennis Moes Portfolio`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-<<<<<<< HEAD
-=======
-  plugins: [`gatsby-plugin-netlify-cms`],
->>>>>>> bcae0861a7b7b6ced200eedb2e86536f876ee1bc
+  plugins: [],
 }
